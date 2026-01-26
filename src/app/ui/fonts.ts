d80801ae4175b18b1d6d26fe9d24ed8c0f97cbb0
Michał Mierzwa <>
2026-01-26 11:25:57 +0100

    Add routes

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Nunito } from "next/font/google";
+import { Changa_One } from "next/font/google";
+
+export const nunito = Nunito({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const changaOne = Changa_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
